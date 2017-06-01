103f557f9f3d97ad582a85f94092f32e43f9d5f9
mughees605 <mughees605@gmail.com>
2017-06-02 03:14:19 +0500

    adding utils

--- eventEmitter/app.js
+++ eventEmitter/app.js
@@ -1,6 +1,27 @@
 var events = require('events')
-var myEmitter = new events.EventEmitter();
-myEmitter.on('someEvent',function(mssg){
-console.log(mssg);
+var util = require('util')
+// var myEmitter = new events.EventEmitter();
+// myEmitter.on('someEvent',function(mssg){
+// console.log(mssg);
+// })
+// myEmitter.emit('someEvent',"hello")
+
+var Person = function(name){
+this.name = name
+}
+
+util.inherits(Person,events.EventEmitter);
+// first argument which constructor wants to inherit and second which we want to inherit this time events
+var James = new Person('james');
+var Mughees = new Person('mughees')
+
+var People = [James,Mughees]
+
+People.forEach(function(person){
+    person.on('speak',function(mssg){
+        console.log(person.name + " " + mssg ) 
+    })
 })
-myEmitter.emit('someEvent',"hello")
+
+James.emit('speak',"hey dude");
+Mughees.emit('speak',"hello")
