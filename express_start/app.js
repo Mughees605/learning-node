0146a5a5ec1daaea57af8595e8a56c8a8a3be056
mughees605 <mughees605@gmail.com>
2017-06-03 14:43:23 +0500

    setting params

--- express_start/app.js
+++ express_start/app.js
@@ -8,4 +8,8 @@ app.get('/contact',function(req,res){
     res.send('this is the contact page')
 })
 
+app.get('/profile/:id',function(req,res){
+    res.send('id ' + req.params.id)
+})
+
 app.listen(3000)
